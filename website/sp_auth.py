19b16cce60d39977f09df942233755bde0c4116d
Anna Konieczka <konieczka.ania@gmail.com>
2023-12-18 11:38:29 +0100

    spotify auth rewritten without spotipy lib

--- website/sp_auth.py
+++ website/sp_auth.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
-import spotipy
 from website.spotify_data.sp_get_library import sp_get_library
-from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
 import time
+import requests
+import urllib.parse
+import base64
 from flask import Flask, request, url_for, session, redirect
 from website.spotify_data.spotify_database import initialize_spotify_database, create_spotify_database
 from website.spotify_data.sp_get_artists_genres import sp_get_artists_genres
@@ -13,64 +14,87 @@ sp_auth = Blueprint('sp_auth', '__name__')
 
 load_dotenv()
 
-TOKEN_INFO = "token_info"
+
 
 @sp_auth.route('/')
-def login():
-	auth_url = create_spotify_oauth().get_authorize_url()
-	return redirect(auth_url)
+def request_authorization():
+	
+    #requesting authorization from Spotify to access data (get request to the authorize endpoint)
+    #using client id from app registration on spotify dev
 
+    client_id = os.getenv("CLIENT_ID")
+    response_type = 'code'
+    redirect_uri = 'http://127.0.0.1:5000/sp_auth/redirect'
+    scope = 'user-library-read playlist-read-private user-follow-read'
+    state = 'fgfrgwgawgwwe' #ZAMIENIĆ NA RANDOM STRING
+
+    params = {'client_id': client_id, 'response_type': response_type, 'redirect_uri': redirect_uri, 'scope': scope, 'state': state}
+    authorize_url = 'https://accounts.spotify.com/authorize'
+    spotify_login_page_url = authorize_url + '?' + urllib.parse.urlencode(params)
+
+    # dodać, że jeśli status jest inny niż 200, to przekierować gdzieś tam?
+
+    return redirect(spotify_login_page_url)
+
+#our endpoint to which spotify sends back code and state
 @sp_auth.route('/redirect')
 def redirect_page():
-	session.clear()
-	code = request.args.get('code')
-	token_info = create_spotify_oauth().get_access_token(code)
-	session[TOKEN_INFO] = token_info
-	return redirect(url_for('sp_auth.library_main', external = True))
 
-@sp_auth.route('/Library_Main')
-def library_main():
-	try:
-		token_info = get_token()
-	except:
-		print("User not logged in")
-		return redirect('/')
+    #user accepted app's request and logged in
+    #retrieving query parameters (code and state) from spotify callback
 
-	sp = spotipy.Spotify(auth=token_info['access_token'])
+    state_received = request.args.get("state")
 
-	# initialize_spotify_database()
-	# print("Database created")
+    # if state_received != state:
+    #     pass #jakoś zakończyć proces autoryzacji
+    # else:
+    #     print('Go on')
+    
+    try:
+        code = request.args.get("code")
 
-	# saved_tracks_library, artists_uris = sp_get_library(sp)
-	# print("Tracks downloaded")
-	
-	# artists_uris_genres = sp_get_artists_genres(sp, artists_uris)
-	# print("Genres downloaded")
-
-	# create_spotify_database(saved_tracks_library, artists_uris_genres)
-	# print("Database populated")
-
-	return ("Success")
+    except:
+        error = request.args.get("error")
 
 
-def get_token():
-	token_info = session.get(TOKEN_INFO, None)
-	if not token_info:
-		redirect(url_for('login', external = False))
+    #exchanging authorization code for an access token - post request to the api/token endpoint
 
-	now = int(time.time())
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    redirect_uri = 'http://127.0.0.1:5000/sp_auth/redirect'
+    grant_type = 'authorization_code'
 
-	is_expired = token_info['expires_at'] - now < 60
-	if(is_expired):
-		spotify_oauth = create_spotify_oauth()
-		token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
-	return token_info
+    params = {'grant_type': grant_type, 'code': code, 'redirect_uri': redirect_uri}
+    get_token_base_url = 'https://accounts.spotify.com/api/token'
+    get_token_url = get_token_base_url + '?' + urllib.parse.urlencode(params)
 
 
-def create_spotify_oauth():
-	return SpotifyOAuth(
-		client_id = os.getenv("CLIENT_ID"),
-		client_secret = os.getenv("CLIENT_SECRET"),
-		redirect_uri = url_for('sp_auth.redirect_page', _external = True),
-		scope = 'user-library-read playlist-read-private user-follow-read'
-		)
+    def convert_to_base64_str(data):
+
+        data_bytes = data.encode('ascii')
+        data_base64 = base64.b64encode(data_bytes)
+        data_base64_str = data_base64.decode()
+        return data_base64_str
+
+    headers = {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + convert_to_base64_str(client_id + ':' + client_secret)
+    }
+
+    access_token_response = requests.post(get_token_url, headers=headers)
+
+    print(access_token_response)
+
+    access_token_response_json = access_token_response.json()
+    print(access_token_response_json)
+    print(type(access_token_response_json))
+
+    access_token = access_token_response_json['access_token']
+    refresh_token = access_token_response_json['refresh_token']
+    expires_in = access_token_response_json['expires_in']
+
+    return ('You have successfully logged in to Spotify. CREATE LIBRARY.')
+
+
+def refresh_token():
+    pass
